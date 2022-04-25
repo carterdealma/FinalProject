4abb511cb18698d3eea17eb75ecc191fc5f49479
carterdealma <85889648+carterdealma@users.noreply.github.com>
2022-04-25 12:35:58 -0600

    Added panel and layout to FinalPanel

--- src/fin/view/FinalPanel.java
+++ src/fin/view/FinalPanel.java
@@ -1,20 +1,30 @@
 package fin.view;
 
-import java.awt.Color;
-
-import javax.swing.*;
-
 import fin.controller.FinalController;
+
+import java.awt.*;
+import javax.swing.*;
 
 public class FinalPanel extends JPanel
 {
 	private FinalController controller;
+	
+	private JPanel cardPanel;
+	private SpringLayout layout;
+	private JLabel houseImageLabel;
+	private JLabel playerImageLabel;
+	private ImageIcon playerFirstCard;
+	private ImageIcon houseFirstCard;
 	
 	public FinalPanel(FinalController controller)
 	{
 		super();
 		
 		this.controller = controller;
+		this.layout = new SpringLayout();
+		this.cardPanel = new JPanel(new GridLayout (0,1));
+		this.playerFirstCard = new ImageIcon();
+		this.houseFirstCard = new ImageIcon();
 		
 		setupPanel();
 		setupListeners();
@@ -25,6 +35,8 @@ public class FinalPanel extends JPanel
 	{
 		Color prettyPink = new Color(255, 204, 255);
 		this.setBackground(prettyPink);
+		this.setLayout(layout);
+		this.add(cardPanel);
 	}
 	
 	public void setupListeners()
@@ -34,6 +46,9 @@ public class FinalPanel extends JPanel
 	
 	public void setupLayout()
 	{
-		
+		layout.putConstraint(SpringLayout.NORTH, cardPanel, 30, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.WEST, cardPanel, 30, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.SOUTH, cardPanel, -30, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.EAST, cardPanel, -30, SpringLayout.EAST, this);
 	}
 }
