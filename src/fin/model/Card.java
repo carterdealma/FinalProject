bb0109368af76a55fe7d3b3f3ce0a4acbddd25ab
carterdealma <85889648+carterdealma@users.noreply.github.com>
2022-04-25 13:46:38 -0600

    Added constructor, getters, and setters to Card

--- src/fin/model/Card.java
+++ src/fin/model/Card.java
@@ -2,5 +2,29 @@ package fin.model;
 
 public class Card
 {
-	private String value;
+	private String name;
+	private int value;
+	private String suit;
+	
+	public Card(String name, int value, String suit)
+	{
+		this.name = name;
+		this.value = value;
+		this.suit = suit;
+	}
+	
+	private String getName()
+	{
+		return this.name;
+	}
+	
+	private int getValue()
+	{
+		return this.value;
+	}
+	
+	private String getSuit()
+	{
+		return this.suit;
+	}
 }
