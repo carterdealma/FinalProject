61cf6e685175f4c2c6bfc181faa518ede4919007
carterdealma <85889648+carterdealma@users.noreply.github.com>
2022-04-25 11:41:37 -0600

    Created frame & panel for GUI

---
+++ src/finalproj/view/FinalFrame.java
@@ -0,0 +1,18 @@
+package finalproj.view;
+
+import finalproj.controller.FinalController;
+
+import javax.swing.*;
+
+public class FinalFrame
+{
+	private FinalController controller;
+	private FinalPanel panel;
+	
+	public FinalFrame(FinalController controller)
+	{
+		super();
+		
+		this.controller = controller;
+	}
+}
---
+++ src/finalproj/view/FinalPanel.java
@@ -0,0 +1,10 @@
+package finalproj.view;
+
+import finalproj.controller.FinalController;
+
+import javax.swing.*;
+
+public class FinalPanel extends JPanel
+{
+	
+}
