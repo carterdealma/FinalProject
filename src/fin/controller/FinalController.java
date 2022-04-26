d7e16eae8f009cd5232b49d4a8eb204d56029776
carterdealma <carterdealma@gmail.com>
2022-04-25 18:35:37 -0600

    Added Card and ArrayList of Cards to Controller

--- src/fin/controller/FinalController.java
+++ src/fin/controller/FinalController.java
@@ -1,19 +1,23 @@
 package fin.controller;
 
 import fin.view.*;
+import fin.model.*;
+
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class FinalController
 {
 	private FinalFrame frame;
+	private Card card;
+	private ArrayList<Card> cardList;
 	
 	public FinalController()
 	{
 		this.frame = new FinalFrame(this);
-		
-		prepareGame();
 	}
 	
-	public void prepareGame()
+	public void start()
 	{
 		
 	}
