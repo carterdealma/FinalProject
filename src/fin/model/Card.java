cba2ebe58daaaaf859a8df073af99eee93c0d717
carterdealma <carterdealma@gmail.com>
2022-04-25 18:11:15 -0600

    Fixed syntax error

--- src/fin/model/Card.java
+++ src/fin/model/Card.java
@@ -2,29 +2,24 @@ package fin.model;
 
 public class Card
 {
-	private String name;
 	private int value;
 	private String suit;
 	
 	public Card(String name, int value, String suit)
 	{
-		this.name = name;
 		this.value = value;
 		this.suit = suit;
 	}
 	
-	private String getName()
-	{
-		return this.name;
-	}
-	
-	private int getValue()
+	public int getValue()
 	{
 		return this.value;
 	}
 	
-	private String getSuit()
+	public String getSuit()
 	{
 		return this.suit;
 	}
+	
+	
 }
