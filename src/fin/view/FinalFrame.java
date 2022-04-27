d7ca76da876258b68651ff6230a3b5883f296539
carterdealma <carterdealma@gmail.com>
2022-04-27 15:03:25 -0600

    Added javadoc comments to FinalFrame

--- src/fin/view/FinalFrame.java
+++ src/fin/view/FinalFrame.java
@@ -4,11 +4,29 @@ import javax.swing.*;
 
 import fin.controller.FinalController;
 
+/**
+ * GUI Frame class for the game of Blackjack.
+ * @author Carter Ma
+ *
+ */
 public class FinalFrame extends JFrame
-{
+{	
+	//Data members for the JFrame subclass instance
+	
+	/**
+	 * A reference to the FinalController instance.
+	 */
 	private FinalController controller;
+	
+	/**
+	 * The instance of the panel used for playing the game.
+	 */
 	private FinalPanel panel;
 	
+	/**
+	 * Builds an instance of the FinalFrame with a reference to the FinalController instance.
+	 * @param controller A reference to a FinalController to share with the FinalPanel instance.
+	 */
 	public FinalFrame(FinalController controller)
 	{
 		super();
@@ -19,6 +37,9 @@ public class FinalFrame extends JFrame
 		setupFrame();
 	}
 	
+	/*
+	 * Configures the JFrame window subclass to add the panel, have a title, set the size, close when exited, and be visible to the user.
+	 */
 	public void setupFrame()
 	{
 		this.setContentPane(panel);
